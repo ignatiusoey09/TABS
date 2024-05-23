c3c978092e937b91876996ffe623bae03fee5c2a
ignatiusoey09 <156410331+ignatiusoey09@users.noreply.github.com>
2024-05-23 21:07:41 +0800

    added background modal to navbar

--- client/src/app/components/navbar.tsx
+++ client/src/app/components/navbar.tsx
@@ -8,17 +8,25 @@ import NavbarButton from './navbar_button';
 
 
 export default function Navbar() {
+    const ModalOverlay = () => (
+        <div 
+            className='flex fixed top-0 right-0 bottom-0 left-0 bg-black/50 z-30'
+        />
+    );
     return (
-        <div className='flex flex-col h-screen w-4/6 p-2 bg-tembu-green'>
-            <h2 className='basis-1/12 mt-4 text-center text-white text-2xl font-family-metrophobic'>TABS</h2>
-            <div className='flex flex-col basis-7/12 justify-center'>
-                <NavbarButton icon={ <ProfileIcon height={50} width={50} />} text='Profile'/>
-                <NavbarButton icon={ <BookingIcon height={50} width={50} />} text='Bookings'/>
-                <NavbarButton icon={ <NewsIcon height={50} width={50} />} text='News'/>
-                <NavbarButton icon={ <ReportIcon height={50} width={50} />} text='Report'/>
+        <>
+            <div className='flex flex-col fixed h-screen w-4/6 p-2 bg-tembu-green z-40'>
+                <h2 className='basis-1/12 mt-4 text-center text-white text-2xl font-family-metrophobic'>TABS</h2>
+                <div className='flex flex-col basis-7/12 justify-center'>
+                    <NavbarButton icon={ <ProfileIcon height={50} width={50} />} text='Profile'/>
+                    <NavbarButton icon={ <BookingIcon height={50} width={50} />} text='Bookings'/>
+                    <NavbarButton icon={ <NewsIcon height={50} width={50} />} text='News'/>
+                    <NavbarButton icon={ <ReportIcon height={50} width={50} />} text='Report'/>
+                </div>
+                <div className='basis-3/12'/>
+                <NavbarButton icon={ <AboutIcon height={50} width={50} />} text='About'/>
             </div>
-            <div className='basis-3/12'/>
-            <NavbarButton icon={ <AboutIcon height={50} width={50} />} text='About'/>
-        </div>
+            <ModalOverlay />
+        </>
     )
 }
--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,12 +1,15 @@
 'use client'
 
 import Bars from '../../../public/threebars.svg'
+import Navbar from '../components/navbar';
 
 export default function Dashboard() {
     return (
         <div className='h-screen bg-gray-200'>
             <div className='flex flex-row h-16 bg-white place-items-center'>
-                <Bars className='basis-1/12 ml-1' />
+                <button>
+                    <Bars className='basis-1/12 ml-1' />
+                </button>
                 <h2 className='basis-10/12 text-xl font-family-metrophobic text-title-gray text-center'>TABS</h2>
                 <div className='basis-1/12'/>
             </div>
