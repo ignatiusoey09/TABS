0fa35f6783dfb9be674785990aa37bcbafb5bf6a
Ignatius Oey <oey.ignatius@gmail.com>
2024-05-29 00:42:56 +0800

    added some styling to the calendar

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -2,13 +2,18 @@
 
 import Layout from '../components/layout';
 import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import './Calendar.css';
 
 export default function Dashboard() {
     const Child = () => (
-        <div>
-            <Calendar />
-        </div>
+        <>
+            <div className='w-[90%] h-[50%] self-center'>
+                <Calendar calendarType='gregory' formatShortWeekday={(locale, date) => [ `S`, `M`, `T`, `W`, `T`, `F`, `S` ][date.getDay()]}/>
+            </div>
+            <div className='border-2 mt-6 grow'>
+                <h1>time slots here</h1>
+            </div>
+        </>
     );
 
     return (
