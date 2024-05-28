e214f35e9431e08b65abf31c6eebbc736457f73c
Ignatius Oey <oey.ignatius@gmail.com>
2024-05-28 22:23:37 +0800

    added react-calendar to /dashboard

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,11 +1,16 @@
 'use client'
 
 import Layout from '../components/layout';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 export default function Dashboard() {
     const Child = () => (
-        <h1 className='text-center mt-60 text-2xl'>TESTING 123</h1>
+        <div>
+            <Calendar />
+        </div>
     );
+
     return (
         <Layout children={<Child />} />
     );
