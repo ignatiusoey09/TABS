e8dbd71918f33a1b0dff4bc75b3eec2461f23963
Ignatius Oey <oey.ignatius@gmail.com>
2024-05-29 22:38:52 +0800

    increased calendar font size and removed >> buttons

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -3,18 +3,34 @@
 import Layout from '../components/layout';
 import Calendar from 'react-calendar';
 import './Calendar.css';
+import { useState } from 'react';
+
+type ValuePiece = Date | null;
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 export default function Dashboard() {
+
+    const [calendarValue, onChange_calendarValue] = useState<Value>(new Date());
+
     const Child = () => (
         <>
             <div className='w-[90%] h-[50%] self-center'>
-                <Calendar calendarType='gregory' formatShortWeekday={(locale, date) => [ `S`, `M`, `T`, `W`, `T`, `F`, `S` ][date.getDay()]}/>
+                <Calendar 
+                    calendarType='gregory' 
+                    formatShortWeekday={(locale, date) => [ `S`, `M`, `T`, `W`, `T`, `F`, `S` ][date.getDay()]}
+                    value={calendarValue}
+                    onChange={onChange_calendarValue}
+                    next2Label={null}
+                    prev2Label={null}
+                />
             </div>
             <div className='border-2 mt-6 grow'>
                 <h1>time slots here</h1>
             </div>
         </>
     );
+
+    console.log(calendarValue);
 
     return (
         <Layout children={<Child />} />
