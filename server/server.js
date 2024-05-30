e92f54a28af159327e583b3e2686a93b2a6ecf9d
Ignatius Oey <oey.ignatius@gmail.com>
2024-05-30 21:08:06 +0800

    added function to populate database with timeslots

--- server/server.js
+++ server/server.js
@@ -70,6 +70,59 @@ app.post("/api/book", async (req, res) => {
     }
 })
 
+/**
+ * RUN WITH CAUTION
+ */
+async function populateMonth() {
+    //desgining a function to populate the mongodb bookings table
+
+    var today = new Date();
+    var currMonth = today.getMonth();
+    var currYear = today.getFullYear();
+
+    //there is some time conversion shenanigans here as the date gets converted to UTC
+    //instead of local and i cant find a good way to stop that
+    const startOfMonth = new Date(currYear, currMonth, 1);
+    const endOfMonth = new Date(currYear, currMonth + 1, 0);
+    
+    try {
+        await client.connect();
+        const coll = client.db("tabs_main").collection("bookings");
+        var d = startOfMonth;
+        const documentsArr = [];
+
+        while (d <= endOfMonth) {
+            dateString = d.toDateString();
+            console.log(dateString);
+
+            const doc = {
+                date: dateString,
+                slots: [
+                    {time: "8:00 AM", is_booked: false},
+                    {time: "10:00 AM", is_booked: false},
+                    {time: "12:00 PM", is_booked: false},
+                    {time: "2:00 PM", is_booked: false},
+                    {time: "4:00 PM", is_booked: false},
+                    {time: "6:00 PM", is_booked: false},
+                    {time: "8:00 PM", is_booked: false},
+                ]
+            }
+
+            documentsArr.push(doc);
+            var newDate = d.setDate(d.getDate() + 1);
+            d = new Date(newDate);
+        }
+
+        await coll.insertMany(documentsArr);
+        client.close();
+
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+//populateMonth();
+
 app.listen(PORT, () => {
     console.log(`started on port ${PORT}`);
-})
+});
