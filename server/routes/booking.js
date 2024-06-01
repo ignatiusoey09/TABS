afa3d17f8116f24a5c17d51c949bd657154ddd32
Ignatius Oey <oey.ignatius@gmail.com>
2024-06-01 20:53:06 +0800

    implemented auth middlware for bookings routes

--- server/routes/booking.js
+++ server/routes/booking.js
@@ -1,7 +1,9 @@
 const { getDateTimeslots } = require("../controllers/bookingsControllers");
+const requireAuth = require("../middleware/requireAuth");
 
 const express = require("express");
 const router = express.Router();
+router.use(requireAuth);
 
 //make bookings route
 router.post('/get_date_timeslots', getDateTimeslots);
