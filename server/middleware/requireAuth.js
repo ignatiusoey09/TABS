987a6fe0b8c5a956735141623ea4cb513add33f3
Ignatius Oey <oey.ignatius@gmail.com>
2024-06-01 20:51:07 +0800

    added some comments

--- server/middleware/requireAuth.js
+++ server/middleware/requireAuth.js
@@ -5,18 +5,23 @@ const requireAuth = async (req, res, next) => {
 
     const { authorization } = req.headers;
 
+    //check if jwt is included in request
     if (!authorization) {
         return res.status(401).json({error: "Access Denied"});
     }
 
+    //format: "Bearer {jwt}"
     const token = authorization.split(" ")[1];
 
     try {
+        //jwt payload contains user id
         const {_id} = jwt.verify(token, process.env.SECRET);
-        req.user = await User.findOne({_id}).select({_id, email, role});
+        req.user = await User.findOne({_id}).select(['email', 'role']);
         next();
     } catch (e) {
         console.log(e);
         res.status(401).json({error: "Access Denied"});
     }
 }
+
+module.exports = requireAuth;
