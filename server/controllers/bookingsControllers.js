fe0917a1f3c3d0e65e22712c0f14576a570035da
Ignatius Oey <oey.ignatius@gmail.com>
2024-06-02 15:34:39 +0800

    added makeBooking function

--- server/controllers/bookingsControllers.js
+++ server/controllers/bookingsControllers.js
@@ -25,4 +25,34 @@ const getDateTimeslots = async (req, res) => {
     }
 }
 
-module.exports = { getDateTimeslots, };
+const makeBooking = async (req, res) => {
+    let query_date = req.body["date"];
+    let query_time = req.body["time"];
+
+    try {
+        const timeslot_array = await Booking.findOne({date: query_date});
+
+        //occurs if populateMonth function has not been run yet
+        if (timeslot_array == null) {
+            throw new Error("timeslots not out yet");
+        }
+
+        for (ts in timeslot_array) {
+            if (ts["time"] == query_time) {
+                if (ts["is_booked"]) {
+                    throw new Error("timeslot already booked");
+                } else {
+                    await Booking.findOneAndUpdate({"slots._id":ts._id}, {$set: {"slots.$.is_booked": true}});
+                    res.status(200).json({status: "successful"});
+                }
+            }
+        }
+
+
+    } catch (e) {
+        console.log(e);
+        res.status(400).json({error: e});
+    }
+}
+
+module.exports = { getDateTimeslots, makeBooking };
