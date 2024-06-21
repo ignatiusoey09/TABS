6d81d57507421aa0b711c216216ab7460f5ed376
Ignatius Oey <oey.ignatius@gmail.com>
2024-06-21 23:53:52 +0800

    added responsiveness for medium screens

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -18,22 +18,24 @@ export default function Home() {
   }
 
   //styling input boxes when error occurs
-  const styling = error ? "border-2 border-red-400" : "border-1";
+  const error_input_style = error ? "border-2 border-red-400" : "border-1";
+  const input_style = `h-8 lg:h-7 ${error_input_style}`;
 
   //styling for form element
-  const form_style_lg = "lg:grow-0 lg:mt-[10%] lg:items-start lg:w-[600px]";
-  const form_style = `grow flex flex-col mt-[60%] items-center ${form_style_lg}`;
+  const form_style_lg = "lg:grow-0 lg:ml-14 lg:mt-[8%] lg:items-start lg:w-[600px]";
+  const form_style_md = "md:mt-[45%] md:px-28";
+  const form_style = `grow flex flex-col mt-[60%] items-center ${form_style_md} ${form_style_lg}`;
 
   return (
     <div className="flex flex-row">
       <Image className="hidden lg:block mt-6 ml-4" src="/Tempo_Photo.png" alt="Teeny table photo" width={600} height={200}/>
-      <form className="grow flex flex-col mt-[60%] items-center lg:mt-[10%] lg:items-start lg:mx-24" onSubmit={ handleSubmit }>
+      <form className={form_style} onSubmit={ handleSubmit }>
         <h2 className="text-center text-6xl text-title-gray">TABS</h2>
         <h1 className="text-center mt-1 text-title-gray">Tembusu Abbey Booking System</h1>
-          <div className="flex flex-col items-center gap-y-3 mt-10 w-full lg:items-start">
+          <div className="flex flex-col items-center gap-y-3 mt-10 w-full lg:items-start lg:mt-16">
             {error && <h3 className="text-red-400 ">{error}</h3>}
-            <input type="email" name="email" className={styling} placeholder="Email:" required/>
-            <input type="password" name="password" className={styling} placeholder="Password:"  required/>
+            <input type="email" name="email" className={input_style} placeholder="Email:" required/>
+            <input type="password" name="password" className={input_style} placeholder="Password:"  required/>
           </div>
         <button
           className={`${isLoading ? 'hidden' : 'visible'} bg-tembu-lightgreen rounded text-white mt-10 w-[60%] lg:w-[70%]`}
