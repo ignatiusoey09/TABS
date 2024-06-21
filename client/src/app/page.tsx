e3166cae48c02f403722a5b6f46805ca61ca02e9
Ignatius Oey <oey.ignatius@gmail.com>
2024-06-22 00:24:25 +0800

    fixed loading spinner not centering

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -19,16 +19,16 @@ export default function Home() {
 
   //styling input boxes when error occurs
   const error_input_style = error ? "border-2 border-red-400" : "border-1";
-  const input_style = `h-8 lg:h-7 ${error_input_style}`;
+  const input_style = `h-8 w-[70%] lg:h-7 lg:w-full ${error_input_style}`;
 
   //styling for form element
-  const form_style_lg = "lg:grow-0 lg:ml-14 lg:mt-[8%] lg:items-start lg:w-[600px]";
+  const form_style_lg = "lg:ml-14 lg:px-0 lg:mt-[8%] lg:items-start";
   const form_style_md = "md:mt-[45%] md:px-28";
   const form_style = `grow flex flex-col mt-[60%] items-center ${form_style_md} ${form_style_lg}`;
 
   return (
-    <div className="flex flex-row">
-      <Image className="hidden lg:block mt-6 ml-4" src="/Tempo_Photo.png" alt="Teeny table photo" width={600} height={200}/>
+    <div className="flex flex-row h-screen">
+      <Image className="hidden lg:block my-6 ml-4" src="/Tempo_Photo.png" alt="Teeny table photo" width={600} height={200}/>
       <form className={form_style} onSubmit={ handleSubmit }>
         <h2 className="text-center text-6xl text-title-gray">TABS</h2>
         <h1 className="text-center mt-1 text-title-gray">Tembusu Abbey Booking System</h1>
@@ -43,8 +43,9 @@ export default function Home() {
         >
           Login
         </button>
-        <MoonLoader className="mt-8 lg:mx-32" loading={isLoading} />
+        <MoonLoader className="mt-8 self-center lg:mx-32" loading={isLoading} />
       </form>
+      <div className="invisible lg:visible lg:w-[20%]"></div>
     </div>
   );
 }
