f9b004cbcbaf9dcd1de82c98401e381fa81dd247
ignatiusoey09 <156410331+ignatiusoey09@users.noreply.github.com>
2024-08-12 20:39:56 +0800

    Responsiveness (#23)
    * small style changes to bookings page
    
    * added short announcement previews, and a modal for full message
    
    * fixed mobile view for qm profile
    
    * fixed mobile view for announcements and overlays

--- client/src/app/(profiles)/student/page.tsx
+++ client/src/app/(profiles)/student/page.tsx
@@ -64,7 +64,7 @@ export default function Profile() {
      */
     return (
         <Layout>
-            <div className="flex flex-col w-full h-screen p-6 pl-14">
+            <div className="flex flex-col max-w-screen h-full p-2 mt-2 ml-2 lg:mt-6 lg:ml-14">
                 <div>
                     <h2 className="text-3xl">
                         {user.name}
@@ -74,7 +74,7 @@ export default function Profile() {
                     </h2>
                 </div>
                     
-                <div className="mt-20 w-[70%] h-full">
+                <div className="mt-20 w-[95%] h-full lg:w-[70%]">
                     <h1>My Bookings</h1>
                     <MoonLoader className="mx-auto mt-20" loading={isLoading}/>
                     {!isLoading && <div className="flex flex-col space-y-4 overflow-y-auto h-64 z-0">
